a19af8248a3053384d3505bc2dec6cc5d6f7d743
unknown <rahulchoudhary0775@gmail.com>
2022-09-25 19:10:13 +0530

    fixed phone css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import HomeScreen from "./HomeScreen";
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen/>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
