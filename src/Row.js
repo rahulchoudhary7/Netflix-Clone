62df1acae03672ccfdcc4b7bd58e83b4db7b3ec1
unknown <rahulchoudhary0775@gmail.com>
2022-09-27 13:12:56 +0530

    fixed some issues

--- src/Row.js
+++ src/Row.js
@@ -7,7 +7,6 @@ import movieTrailer from "movie-trailer";
 function Row({title, fetchUrl, isLargeRow = false}) {
 
     const [movies, setMovies] = useState([ ]);
-
     const base_url = "https://image.tmdb.org/t/p/original/"
 
     useEffect(()=>{
@@ -25,14 +24,16 @@ function Row({title, fetchUrl, isLargeRow = false}) {
             setTrailerUrl("");
         }
         else{
-            movieTrailer(movie.name || "").then((url) =>{
+            movieTrailer(movie?.name || "").
+            then((url) =>{
                 const urlParams = new URLSearchParams(new URL(url).search)
                 setTrailerUrl(urlParams.get('v'));
-            }).catch(()=>{
-                alert('Temporarily unavailable')
+            }).catch((error)=>{
+                console.log(error);
             })
         }
     }
+
 
     const opts ={
         height: "390",
@@ -47,8 +48,8 @@ function Row({title, fetchUrl, isLargeRow = false}) {
         <h2>{title}</h2>
         <div className="row_posters">
                 {movies.map((movie) => (
-                    // (isLargeRow && movie.poster_path) ||
-                    // (!isLargeRow && movie.backdrop_path) ? 
+                    ((isLargeRow && movie.poster_path) ||
+                    (!isLargeRow && movie.backdrop_path)) && (
 
                 <img
                 onClick={()=> handleTrailer(movie)}
@@ -56,17 +57,15 @@ function Row({title, fetchUrl, isLargeRow = false}) {
                 key = {movie.id}
                 src={
                     `${base_url}${
-                    (isLargeRow && movie.poster_path) ? movie.poster_path 
-                    : (!isLargeRow && movie.backdrop_path) ? movie.backdrop_path 
-                    : "https://www.deviantart.com/sicariusftw/art/Death-Note-Movie-Poster-440275106"
+                    isLargeRow ? movie.poster_path : movie.backdrop_path
                 }`} 
                 alt={movie.name} 
                 /> 
-                ))
+                )))
                 }
 
         </div>
-        {trailerUrl && <YouTube videoId="{trailerUrl} opts = {opts}"/>}
+        {trailerUrl && <YouTube videoId={trailerUrl} opts = {opts}/>}
     </div>
   )
 }
